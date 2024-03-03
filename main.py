bfab12897c55c3b97a3874f8e3071b77d62cd090
Aryan-chand <imaryan1803@gmail.com>
2024-03-03 20:14:01 +0530

    block start moves

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 from pygame.locals import *
 
+def draw_block():
+    surface.fill((255,255,255))
+    surface.blit(block,(block_x,block_y))
+    pygame.display.flip()
+
 if __name__ == "__main__":
     pygame.init()
     
     surface = pygame.display.set_mode((1000,500))
     surface.fill((255,255,255))
+    
+    block = pygame.image.load("Resource/block.jpg").convert()
+    block_x=100
+    block_y=100
+    surface.blit(block,(block_x,block_y))
+    
     pygame.display.flip()
     
     running= True 
@@ -17,6 +28,19 @@ if __name__ == "__main__":
                 if event.key == K_ESCAPE:
                     running = False
                     
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+                                        
             elif event.type == QUIT:
                 running = False
     
