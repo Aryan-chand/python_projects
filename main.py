b618b927428f4145b22a95cca5372c87daaefafe
Aryan-chand <imaryan1803@gmail.com>
2024-03-09 23:26:28 +0530

    eat apple

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 Size = 40
 
@@ -15,15 +16,27 @@ class Apple :
         self.parent_screen.blit(self.image,(self.x,self.y))
         pygame.display.flip()
         
+    def move(self):
+        self.x = random.randint(0,24)*Size
+        self.y = random.randint(0,19)*Size
+        
 class Snake:
     
     def __init__(self, parent_screen, length):
-        self.length = length
+        
         self.parent_screen = parent_screen
         self.block = pygame.image.load("Resource/block.jpg").convert()
+        self.direction = "down"
+        
+        self.length = length
         self.x= [Size]*length
         self.y= [Size]*length
-        self.direction = "down"
+        
+    def inc_length(self):
+        self.length+=1
+        self.x.append(-1)
+        self.y.append(-1)
+        
     
     def move_left(self):
         self.direction ="left"
@@ -64,27 +77,31 @@ class Game:
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((1000,800))
-        self.snake = Snake(self.surface ,5)
+        self.snake = Snake(self.surface ,1)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
     
     def is_collision(self , x1 ,y1 , x2, y2):
-        if x1 >= x2 and x1 <= x2 + Size:
+        if x1 >= x2 and x1 < x2 + Size:
             if y1 >= y2 and y1 <= y2 + Size:
                 return True
         return False
     
-    
+    def diplay_score(self):
+        font= pygame.font.SysFont('arial',18)
+        score = font.render(f"Score: {self.snake.length}" , True, (200,200,200))
+        
+        
+        
     def play(self):
         self.snake.walk()
         self.apple.draw()
         
         if self.is_collision(self.snake.x[0],self.snake.y[0],self.apple.x,self.apple.y):
-            print("Collision occoured")
-        
-        
-        
+            self.apple.move()
+            self.snake.inc_length()
+         
     def run(self):
         running= True 
     
