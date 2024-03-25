7d2dedd55a2a6d8b4760bea3159bc52aba18ee8a
Aryan-chand <imaryan1803@gmail.com>
2024-03-25 23:31:02 +0530

    players moves

--- TIC-TAC-TOE/main.py
+++ TIC-TAC-TOE/main.py
@@ -22,3 +22,33 @@ def isBoardFull(board):
         return False
     else:
         return True
+
+def IsWinner(b,l):
+    return(
+    (b[1] == l and b[2] == l and b[3] == l) or
+    (b[4] == l and b[5] == l and b[6] == l) or
+    (b[7] == l and b[8] == l and b[9] == l) or
+    (b[1] == l and b[4] == l and b[7] == l) or
+    (b[2] == l and b[5] == l and b[8] == l) or
+    (b[3] == l and b[6] == l and b[9] == l) or
+    (b[1] == l and b[5] == l and b[9] == l) or
+    (b[3] == l and b[5] == l and b[7] == l)
+    )
+
+def playerMove():
+    run = True
+    while run:
+        move = input("please select a position to enter the X between 1 to 9: ")
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry, this space is occupied')
+            else:
+                print('please type a number between 1 and 9')
+        
+        except:
+            print('Please type a number')
