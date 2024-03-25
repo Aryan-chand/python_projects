920f03fc9f5d8d9638bebddfbe6833aaa9cef194
Aryan-chand <imaryan1803@gmail.com>
2024-03-25 23:31:32 +0530

    computers move

--- TIC-TAC-TOE/main.py
+++ TIC-TAC-TOE/main.py
@@ -52,3 +52,37 @@ def playerMove():
         
         except:
             print('Please type a number')
+
+def computerMove():
+    possibleMoves = [ x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardcopy = board[:]
+            boardcopy[i] = let
+            if IsWinner(boardcopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgesOpen.append(i)
+
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
